583845337b7b38e808968ed85158514bf77f0c18
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-26 09:21:10 +0300

    Basic DB usage

--- Kauppakanta.cs
+++ Kauppakanta.cs
@@ -19,4 +19,38 @@ class Kauppakanta
             CommandForCreateTable.ExecuteNonQuery();
         }
     }
+    //Metodi, jolla lisätään asiakas tietokantaan
+    public void LisaaAsiakas(string nimi)
+    {
+        using (var connection = new SqliteConnection(_connectionstring))
+        {
+            connection.Open();
+            //Lisätään asiakas tietokantaan
+            var CommandForAddingCustomer = connection.CreateCommand();
+            CommandForAddingCustomer.CommandText = @"INSERT INTO Asiakkaat (nimi) VALUES ($nimi)";
+            CommandForAddingCustomer.Parameters.AddWithValue("$nimi", nimi);
+            CommandForAddingCustomer.ExecuteNonQuery();
+        }
+    }
+
+    public Dictionary<int, string> HaeAsiakkaat()
+    {
+        using (var connection = new SqliteConnection(_connectionstring))
+        {
+            connection.Open();
+            //Haetaan kaikki asiakkaat tietokannasta
+            var CommandForGettingCustomers = connection.CreateCommand();
+            CommandForGettingCustomers.CommandText = @"SELECT * FROM Asiakkaat";
+            using (var reader = CommandForGettingCustomers.ExecuteReader())
+            {
+                var asiakkaat = new Dictionary<int, string>();
+                while (reader.Read())
+                {
+                    asiakkaat.Add(reader.GetInt32(0), reader.GetString(1));
+                }
+                return asiakkaat;
+            }
+                
+        }
+    }
 }
